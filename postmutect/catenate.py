6397b86e9604b8a2dcd8aeef7f5d7c7401ed8e77
PySean <smsod18@hotmail.com>
2015-07-28 22:58:28 -0500

    Separate function for special case of BAMs generated from individual chromosomes was written, bad designs were removed.

--- postmutect/catenate.py
+++ postmutect/catenate.py
@@ -6,9 +6,9 @@
 """
 import argparse
 import os
+import re
 import sys
 from subprocess import check_output
-from os.path import basename
 try:
     from arguer import makeparser
 except ImportError as I:
@@ -26,7 +26,6 @@ parser.add_argument('-l', '--listing', type=str,
                           'chromosome basis. Uses chrs.list by default.'),
                     default='chrs.list')
 args = parser.parse_args()
-
 """
 Validates the list of chromosome vcf file fragments. Removes paths that
 lead to files of size zero, or that do not exist.
@@ -37,53 +36,81 @@ def chr_validate(chrlist):
 
 """
 Concatenates all vcfs under a directory. 
-By default, does not remove the vcf files.
-At the moment, cleans up after multimutect.py with regards to the 
-creation of a status directory as well as moving status files up into
-said directory.
 """
-def vcf_catenate(parent, reference, gatkpath, listfile, delete):
+def vcf_catenate(directory, reference, gatkpath, listing, delete_fragments):
     cmd = ('java -cp {gatk} org.broadinstitute.gatk.tools.CatVariants'
            ' -assumeSorted -R' 
            ' {ref} -out {{out}} {{vcfs}}').format(gatk=gatkpath, 
                                                 ref=reference)
-    for dirpath, dirnames, filenames in os.walk(parent):
-        for directory in dirnames:
-            d_path = os.path.join(dirpath, directory)
-            listing = listfile
-            if listfile == 'chrs.list':
-                listing = os.path.join(d_path, listing)
-            result_name = directory + '.vcf'
-            status_name = directory + '.list'
+    for dirpath, dirnames, filenames in os.walk(directory):
+        for dir in dirnames:
+            d_path = os.path.join(dirpath, dir)
+            listing = os.path.join(d_path, listing)
+            result_name = dir + '.vcf'
             outpath = os.path.join(dirpath, result_name)
             #Make sure the listing file exists and that this isn't the
             #status directory.
             if os.path.exists(listing):
                 with open(listing, 'r') as chrfile:
-                    chrlist = [os.path.join(d_path, basename(c.strip())) + '.vcf' 
+                    chrlist = [os.path.join(d_path, c.strip()) + '.vcf' 
                                for c in chrfile]
-                    #Keep original chrlist for deletions.
+                    #Keep original chrlist for deletions of possibly empty
+                    #VCF files.
                     realchrs = chr_validate(chrlist)
                     #The list of vcfs to concatenate, each prepended by
                     #-V.
                     vseries = "".join(['-V ' + c + ' ' for c in realchrs])
                     final_cmd = cmd.format(out=outpath, vcfs=vseries)
                     check_output(final_cmd.split())
-                    #Clean up leftover files after combining them if the option
-                    #is specified.
+                    #Clean up leftover files after combining them.
                     #Also cleans up .idx files and directories.
-                    if delete == True:
+                    if delete_fragments == True:
                         map(os.unlink, chrlist)
                         #No index file is created for empty vcfs.
                         map(lambda x: os.unlink(x + '.idx'), realchrs)
-                        if listfile == 'chrs.list':
-                            os.unlink(listing)
+                        os.unlink(listing)
                         os.rmdir(d_path)
+"""
+Smaller concatenation function for the case of a single directory with
+VCFs generated from BAM files each representing single chromosomes.
+"""
+def minicat(directory, reference, gatkpath, listing, delete_fragments):
+    cmd = ('java -cp {gatk} org.broadinstitute.gatk.tools.CatVariants'
+           ' -assumeSorted -R' 
+           ' {ref} -out {{out}} {{vcfs}}').format(gatk=gatkpath, 
+                                                ref=reference)
+    file_list = []
+    if os.path.exists(directory) and os.path.exists(listing):
+        with open(listing, 'r') as listfile:
+            #Convert .bam extension to .vcf.
+            file_vcfs = [re.sub('\.bam', '.vcf', bam) for bam in listfile]
+            file_list = [os.path.join(directory, 
+                                      os.path.basename(vcf.strip()))
+                        for vcf in file_vcfs]
+    else:
+        sys.stderr.write(('Error: {} or {} do not exist.'
+                         ' Please check your command'
+                         ' line').format(directory, listfile))
+        sys.exit(1)
+    #Create series of VCFs to concatenate
+    vseries = "".join(['-V ' + vcf + ' ' for vcf in file_list])
+    outfile = directory + '.vcf'
+    final_cmd = cmd.format(out=outfile, vcfs=vseries)
+    check_output(final_cmd.split())
+    
+    if delete_fragments == True:
+        map(os.unlink, file_list)
+        map(lambda x: os.unlink(x + '.idx'), file_list)
+
+cat_func = vcf_catenate
+if args.listing != 'chrs.list':
+    cat_func = minicat
 
 if os.path.exists(args.gatkpath):
-    vcf_catenate(args.directory, args.reference, args.gatkpath)
+    cat_func(**vars(args))
+#Try again with the default.
 elif os.path.exists('gatk.jar'):
-    vcf_catenate(args.directory, args.reference, args.gatkpath)
+    cat_func(**vars(args))
 else:
     sys.stderr.write('Please provide an existent gatk jar filepath.')
     sys.exit(1)
